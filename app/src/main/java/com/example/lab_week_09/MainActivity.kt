e4d982a0ebaeb2eb9629a2a3f2d02d49bc3a93fa
kayoo18 <92360576+kayoo18@users.noreply.github.com>
2025-11-07 08:19:04 +0700

    Commit No. 3 – UI Element and Theme

--- app/src/main/java/com/example/lab_week_09/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_09/MainActivity.kt
@@ -17,6 +17,9 @@ import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.lab_week_09.ui.theme.LAB_WEEK_09Theme
+import com.example.lab_week_09.ui.theme.OnBackgroundItemText
+import com.example.lab_week_09.ui.theme.OnBackgroundTitleText
+import com.example.lab_week_09.ui.theme.PrimaryTextButton
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -102,16 +105,25 @@ fun HomeContent(
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
 
-                Text(text = stringResource(id = R.string.enter_item))
+                // Title pakai UI Element
+                OnBackgroundTitleText(
+                    text = stringResource(id = R.string.enter_item)
+                )
 
+                // TextField input
                 TextField(
                     value = inputField.name,
                     onValueChange = { onInputValueChange(it) },
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text
+                    )
                 )
 
-                Button(onClick = { onButtonClick() }) {
-                    Text(text = stringResource(id = R.string.button_click))
+                // Button pakai UI Element
+                PrimaryTextButton(
+                    text = stringResource(id = R.string.button_click)
+                ) {
+                    onButtonClick()
                 }
             }
         }
@@ -121,13 +133,18 @@ fun HomeContent(
             Column(
                 modifier = Modifier
                     .padding(vertical = 4.dp)
-                    .fillMaxWidth(),
+                    .fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text(text = student.name)
+
+                // Item text pakai UI Element
+                OnBackgroundItemText(
+                    text = student.name
+                )
             }
         }
     }
+
 }
 
 @Preview(showBackground = true)
